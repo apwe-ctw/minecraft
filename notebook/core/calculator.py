0e59c76a3ead8151d6f4dfb7af8e379704d9279d
apweCTW <apwe.ctw@gs.com>
2025-03-26 15:39:41 +0800

    2025 init commit

---
+++ notebook/core/calculator.py
@@ -0,0 +1,134 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+from core.utils import Fund, Sprite, ActionType
+
+import time
+import random
+import RPi.GPIO as GPIO
+
+@dataclass
+class Calculator:
+    initial_deposit: float = 0
+    contribution_amount: float = 0
+    investment_timespan: int = 0
+    fund: Fund = Fund.HONG_KONG_ETF
+    future_balance: float = 0
+    contribution_period: int = 12
+    compound_period: int = 12
+    sprite: Sprite = Sprite.HEARTBEAT
+    goal: float = 1000000
+
+        
+    def set_lights(self):
+        """
+        Set the lights up!
+        """
+        LED = 17
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(LED, GPIO.OUT) 
+
+        GPIO.output(LED, True)
+        time.sleep(10.0)
+
+        GPIO.cleanup() 
+        return None
+    
+    def get_sprite(self):
+        """
+        Used to retrieve the sprite displayed when target investment reached
+        """
+        return self.sprite.value
+
+    def get_funds(self):
+        """
+        Used to retrieve a list of available funds
+        :return:
+        """
+        return [i.value for i in Fund]
+
+    def get_selected_fund(self):
+        """
+        Retrieves the current fund selected by the user
+        """
+        return {"name": self.fund.name, "returns": self.fund.returns}
+
+    def get_amount(self, field: str):
+        """
+        Used to retrieve values from the calculator. E.g. initial deposit
+        :param field: name of the field
+        :return:
+        """
+        return getattr(self, field)
+
+    def change_fund(self, name: str):
+        """
+        Change the fund selected for the calculation
+        :param name: Name of the fund
+        :return:
+        """
+        self.fund = Fund.value_from(name)
+
+    def change_amount(self, field: str, value, action_string: str):
+        """
+        Used to change a property on the calculator. E.g. initial deposit, contribution amount e.t.c
+        :param field: calculator property
+        :param value: Any
+        :param action_string: ADD | SUBTRACT | SET
+        :return: the updated field value
+        """
+        action = ActionType.value_from(action_string)
+        change = getattr(self, field)
+
+        if action == ActionType.ADD:
+            change += value
+        elif action == ActionType.SUBTRACT:
+            change -= value
+        elif action == ActionType.SET:
+            change = value
+
+        if change >= 0:
+            setattr(self, field, change)
+            return change
+        return getattr(self, field)
+
+    def calculate_future_values(self):
+        """
+        Calculates the future value
+        """
+        current_year = datetime.now().year
+        labels = [year for year in range(current_year, current_year + int(self.investment_timespan))]
+        principal_dataset = {
+            "label": 'Total Principal',
+            "backgroundColor": 'rgb(0, 123, 255)',
+            "data": []
+        }
+        earnings_dataset = {
+            "label": "Total Earnings",
+            "backgroundColor": 'rgb(23, 162, 184)',
+            "data": []
+        }
+
+        self.future_balance = 0
+
+        for time in range(1, self.investment_timespan + 1):
+            principal = self.initial_deposit + (self.contribution_amount * self.contribution_period * time)
+            earnings = 0
+            balance = principal
+            estimated_return = self.fund.returns / 100
+
+            if estimated_return:
+                gains = (1 + estimated_return / self.compound_period) ** (self.compound_period * time)
+                compound_earnings = self.initial_deposit * gains
+                contribution_earnings = self.contribution_amount * (gains - 1) / (estimated_return / self.contribution_period)
+                earnings = compound_earnings + contribution_earnings - principal
+                balance = compound_earnings + contribution_earnings
+
+            self.future_balance = balance
+            principal_dataset['data'].append(round(principal, 2))
+            earnings_dataset['data'].append(round(earnings, 2))
+
+        return {
+            "labels": labels,
+            "datasets": [principal_dataset, earnings_dataset],
+        }
---
+++ notebook/main.py
@@ -0,0 +1,200 @@
+import webview
+from webview import Window
+
+from core.calculator import Calculator
+
+
+def main(win: Window):
+    """
+    Inject starter JavaScript
+    :param win: Window
+    """
+    win.evaluate_js(
+        """
+        (async() => {
+            const ctx = document.getElementById('myChart').getContext('2d');
+            const investment_timespan = document.querySelector('#investment_timespan');
+            const investment_timespan_text = document.querySelector('#investment_timespan_text');
+            const future_balance = document.querySelector('#future_balance');
+            const depositSelector = document.querySelector('#initial_deposit');
+            const estimatedReturnSelector = document.querySelector('#estimated_return');
+            const contributionSelector = document.querySelector('#contribution_amount');
+            const fundSelector = document.querySelector('#fund');
+            const spriteSelector = document.querySelector('#sprite');
+            const funds = await pywebview.api.get_funds();
+
+            const updateChart = async () => {
+                const data = await pywebview.api.calculate_future_values();
+
+                chart.data.labels = data.labels;
+                chart.data.datasets[0].data = data.datasets[0].data;
+                chart.data.datasets[1].data = data.datasets[1].data;
+                chart.update();
+                const balance = await pywebview.api.get_amount('future_balance');
+                future_balance.innerHTML = '$' + balance.toFixed(2);
+                const goal = await pywebview.api.get_amount('goal');
+                
+                if (balance >= goal) {
+                    const sprite = await pywebview.api.get_sprite();
+                    pywebview.api.set_lights();
+                    if (sprite) {
+                        spriteSelector.src = sprite;
+                    }
+                } else {
+                    spriteSelector.src = '';
+                }
+            }
+
+            const changeHandler = (dom, action = 'set') => {
+                const name = dom.getAttribute('id');
+                const min = parseFloat(dom.getAttribute('min'));
+                const max = parseFloat(dom.getAttribute('max'));
+                const oldValue = dom.dataset.value || dom.defaultValue || 0;
+                let newValue = parseFloat(dom.value.replace(/\$/, ''));
+                const step = action === 'set' ? newValue: parseFloat(dom.getAttribute('step')) || 1;
+
+                setTimeout(async () => {
+                    if (isNaN(parseFloat(newValue))) {
+                        newValue = oldValue;
+                    } else {
+                        newValue = await pywebview.api.change_amount(name, step, action);
+                        newValue = newValue < min ? min : newValue > max ? max : newValue;
+                    }
+    
+                    dom.dataset.value = newValue;
+                    dom.value = (dom.dataset.prepend || '') + newValue + (dom.dataset.append || '');
+                    await updateChart();
+                }, 0);
+            };
+            
+            const selectedFund = await pywebview.api.get_selected_fund();
+            investment_timespan.value = await pywebview.api.get_amount('investment_timespan');
+            investment_timespan_text.innerHTML = investment_timespan.value + ' years';
+            depositSelector.dataset.value = await pywebview.api.get_amount('initial_deposit');
+            depositSelector.value = '$' + depositSelector.dataset.value;
+            contributionSelector.dataset.value = await pywebview.api.get_amount('contribution_amount');
+            contributionSelector.value = '$' + contributionSelector.dataset.value;
+            estimatedReturnSelector.dataset.value = selectedFund.returns;
+            estimatedReturnSelector.value = estimatedReturnSelector.dataset.value + '%';
+            
+            depositSelector.addEventListener('change', function(){ changeHandler(this) });
+            fundSelector.addEventListener('change', async function(){ 
+                await pywebview.api.change_fund(this.value);
+                estimatedReturnSelector.value = funds.find(v => v[0] === this.value)[1] + '%';
+                await updateChart();
+            });
+            estimatedReturnSelector.addEventListener('change', function(){ changeHandler(this) });
+            contributionSelector.addEventListener('change', function(){ changeHandler(this) });
+            investment_timespan.addEventListener('change', async function () {
+            
+                changeHandler(this);
+                investment_timespan_text.innerHTML = this.value + ' years';
+                await updateChart();
+            });
+            investment_timespan.addEventListener('input', function () {
+                investment_timespan_text.innerHTML = this.value + ' years';
+            });
+            
+            const contribution_period = await pywebview.api.get_amount('contribution_period');
+            const compound_period = await pywebview.api.get_amount('compound_period');
+            
+            Array.from(document.querySelectorAll('[name="contribution_period"], [name="compound_period"]'))
+            .forEach(radio => {
+            if (radio.name.includes('contribution') && parseInt(radio.value) === contribution_period) {
+                radio.checked = true;
+            } else if (radio.name.includes('compound') && parseInt(radio.value) === compound_period) {
+                radio.checked = true;
+            }
+            radio.addEventListener('change', async function() {
+                if (this.checked) {
+                    await pywebview.api.change_amount(this.getAttribute('name'), parseInt(this.value), 'set');
+                }
+                await updateChart();
+            });
+            });
+
+            Array.from(document.querySelectorAll('[data-counter]'))
+            .forEach(
+                button => button.addEventListener('click', function () {
+                    var field = document.querySelector('[name="' + this.dataset.field + '"]'),
+                        action = this.dataset.counter;
+
+                    if (field) {
+                        changeHandler(field, action);
+                    }
+                })
+            );
+            
+            funds.forEach(fund => {
+                const opt = document.createElement('option');
+                opt.value = fund[0];
+                opt.innerHTML = fund[0];
+                fundSelector.appendChild(opt);
+            });
+
+            fundSelector.value = selectedFund.name;
+            const chartData = await pywebview.api.calculate_future_values();
+            const balance = await pywebview.api.get_amount('future_balance');
+            future_balance.innerHTML = '$' + balance.toFixed(2);
+            
+            const goal = await pywebview.api.get_amount('goal');
+                
+            if (balance >= goal) {
+                const sprite = await pywebview.api.get_sprite();
+                if (sprite) {
+                    spriteSelector.src = sprite;
+                }
+            } else {
+                spriteSelector.src = '';
+            }
+            
+            const chart = new Chart(ctx, {
+                type: 'bar',
+                data: chartData,
+                options: {
+                    legend: {
+                        display: false
+                    },
+                    tooltips: {
+                        mode: 'index',
+                        intersect: false,
+                        callbacks: {
+                            label: function (tooltipItem, data) {
+                                console.log(data.datasets[tooltipItem.datasetIndex]);
+                                return data.datasets[tooltipItem.datasetIndex].label + ': $' + tooltipItem.yLabel;
+                            }
+                        }
+                    },
+                    responsive: true,
+                    scales: {
+                        xAxes: [{
+                            stacked: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Year'
+                            }
+                        }],
+                        yAxes: [{
+                            stacked: true,
+                            ticks: {
+                                callback: function (value) {
+                                    return '$' + value;
+                                }
+                            },
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Balance'
+                            }
+                        }]
+                    }
+                }
+            });
+        })();
+        """
+    )
+    pass
+
+
+if __name__ == '__main__':
+    window = webview.create_window('Financial Calculator', 'web/index.html', js_api=Calculator(), height=800, width=1100)
+    webview.start(main, window)
